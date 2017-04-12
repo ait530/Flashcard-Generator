dcce3107cd63bb48bc9683dd19b3081111118bf4
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-04-12 12:58:35 -0500

    Added initial constructors

---
+++ components.js
@@ -0,0 +1,15 @@
+// BasicCard Constructor
+function BasicCard(front, back) {
+  this.front = front;
+  this.back = back;
+
+
+  
+}
+
+
+// ClozeCard Constructor
+function ClozeCard(text, cloze) {
+  this.text = text;
+  this.cloze = cloze;
+}
