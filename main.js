4a30949c5cfc25a006ee272e5eb8e67ecf417562
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-04-14 00:07:20 -0500

    update pseudocode

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var inquirer = require('inquirer');
-
+// recursion in inquirer see examples.
 
 var basicQuestions = [];
 var basicAnswers = [];
@@ -24,3 +24,11 @@ function ClozeCard(text, cloze) {
 // The full text. This is the entire sentence users need to remember: "George Washington was the first president of the United States."
 // The cloze deletion. This is the text we've chosen to remove: "George Washington".
 // The partial text. This is what we get if we remove the cloze deletion from the full text: "... was the first president of the United States.
+
+
+
+// data.js like the keys.js
+// export the data.js into the keys.js
+
+
+// basis.js  one with cloze.js
