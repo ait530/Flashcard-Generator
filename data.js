f934227eee79150dbd34e0a8c8e37a9ad5da041c
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-04-14 22:25:43 -0500

    putting to github

---
+++ data.js
@@ -0,0 +1,9 @@
+// export -- an object which is shared between all instances of the current module and made accessible through require().
+exports.dataCards = {
+  firstquestion: 'Who was the first president of the United States?',
+  firstanswer: 'George Washington',
+  secondquestion: '..., East, South, West.',
+  secondanswer: 'North',
+  thirdquestion: 'How many square miles is the United States?',
+  thirdanswer: '3.8 million square miles.'
+}
--- main.js
+++ main.js
@@ -1,24 +1,104 @@
 var inquirer = require('inquirer');
+var fs = require('fs');
+var key = require('./data.js');
 // recursion in inquirer see examples.
 
-var basicQuestions = [];
-var basicAnswers = [];
 
-var clozeQuestions = [];
-var clozeAnswers = [];
+var cmdInput = process.argv[2];
+getInput(cmdInput);
+function getInput(cmdInput, args) {
+    if (cmdInput) {
+        switch (cmdInput) {
+        case 'basic':
+            console.log("BASIC CARDS YAH");
+            basicCards();
+            break;
+        case 'cloze':
+            console.log('cloze is running');
+            // if (args) {
+            //     console.log(' Argument passed: ' + args);
+            //     clozeCards(args);
+            // }
+        }
+    }
+}
 
-// BasicCard Constructor
-function BasicCard(front, back) {
-  this.front = front;
-  this.back = back;
+// function runCommand() {
+//     fs.readFile('random.txt', 'utf-8', function (error, data) {
+//         var fileCommands = data.split(',');
+//         getInput(fileCommands[0], fileCommands[1]);
+//     });
+// }
+function logged() {
+    // captures all command line inputs
+    var inputs = process.argv.slice(2).join(" ");
+    // feeeds the  data to the log file
+    // console.log(inputs);
+    // appends data to the log file after each run
+    fs.appendFile("log.txt", "node main.js: " + inputs + "\n", function (error) {
+        if (error) {
+            throw error;
+        }
+        else {
+            console.log(" updated log file! ");
+        }
+    });
+    return true;
 }
 
 
-// ClozeCard Constructor
-function ClozeCard(text, cloze) {
-  this.text = text;
-  this.cloze = cloze;
-}
+
+
+
+
+
+
+
+
+
+
+
+// Function which takes in command line arguments and executes correct function accordigly
+// var runThis = function(argOne, argTwo) {
+//   pick(argOne, argTwo);
+// };
+
+// // MAIN PROCESS
+// // =====================================
+// runThis(process.argv[2], process.argv[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var basicQuestions = [];
+// var basicAnswers = [];
+
+// var clozeQuestions = [];
+// var clozeAnswers = [];
+
+// // BasicCard Constructor
+// function BasicCard(front, back) {
+//   this.front = front;
+//   this.back = back;
+// }
+
+
+
+
+// // ClozeCard Constructor
+// function ClozeCard(text, cloze) {
+//   this.text = text;
+//   this.cloze = cloze;
+// }
 
 
 // The full text. This is the entire sentence users need to remember: "George Washington was the first president of the United States."
